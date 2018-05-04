1a148331e877a3aed67c562e1f1abae31808d712
Cecilia medina <cleanlifellc@gmail.com>
2018-05-04 06:42:20 -0400

    done

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+var friendsArray = [
+	{
+		name: 'Scott',
+		photo: 'https://vignette.wikia.nocookie.net/uncyclopedia/images/8/83/Lame_indian_guy.jpg/revision/latest?cb=20100710013319',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Frank',
+		photo: 'http://4.bp.blogspot.com/-FaDPK3tQ8lc/Ufph5suq1FI/AAAAAAAABm0/pIyZAQoq1qc/s400/ugly-people-funny-pictures-i7.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+
+	
+];
+
+module.exports = friendsArray;
